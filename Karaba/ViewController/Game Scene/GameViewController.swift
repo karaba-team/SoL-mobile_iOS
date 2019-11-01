e0118ebd8dfcbbc889a3d8fb7fbcc2b04cdb1300
Andika Leonardo <leonardodika.13@gmail.com>
2019-11-01 08:16:05 +0700

    Extensions Added

--- Karaba/ViewController/Game Scene/GameViewController.swift
+++ Karaba/ViewController/Game Scene/GameViewController.swift
@@ -122,41 +122,5 @@ class GameViewController: UIViewController, SKViewDelegate{
         }
     }
 }
-extension GameViewController : UICollectionViewDelegate, UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return itemColor.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dragNDropItemCell", for: indexPath) as! DragNDropItemCell
-        cell.testView.backgroundColor = UIColor(hexString: itemColor[indexPath.row])
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        selectedItem(itemIndex: indexPath.row, frame : collectionView.layoutAttributesForItem(at: indexPath)!.frame)
-    }
-    
-}
-extension UIColor {
-    convenience init?(hexString: String) {
-        var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
-        let red, green, blue, alpha: CGFloat
-        switch chars.count {
-        case 3:
-            chars = chars.flatMap { [$0, $0] }
-            fallthrough
-        case 6:
-            chars = ["F","F"] + chars
-            fallthrough
-        case 8:
-            alpha = CGFloat(strtoul(String(chars[0...1]), nil, 16)) / 255
-            red   = CGFloat(strtoul(String(chars[2...3]), nil, 16)) / 255
-            green = CGFloat(strtoul(String(chars[4...5]), nil, 16)) / 255
-            blue  = CGFloat(strtoul(String(chars[6...7]), nil, 16)) / 255
-        default:
-            return nil
-        }
-        self.init(red: red, green: green, blue:  blue, alpha: alpha)
-    }
-}
+
+
